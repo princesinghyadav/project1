1ba6cf3ea67ddd01e701bb76b282841660151826
princesinghyadav <princesinghyadav017@gmail.com>
2025-04-09 18:12:41 +0530

    mixture of projects

---
+++ tailwind-tutorial-spaceing-customization/tailwind-tutorial-spaceing-customization/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+
+    extend: {
+      spacing: {
+        '21': '6rem',
+        '22': '3rem',
+        '13':'13px',
+        'px-8':'8px',
+        'px-100':'100px'
+      }
+    },
+    container: {
+      center: true
+    }
+  },
+  plugins: [],
+}
+
+
